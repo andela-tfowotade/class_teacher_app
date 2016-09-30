030dd122eb49e1df5d3c2688b008e0599c3192c9
temifowotade <temitope.fowotade@andela.com>
2016-09-30 10:36:28 +0100

    test(creating form): Adds features testing for creating forms

--- spec/controllers/teachers_controller_spec.rb
+++ spec/controllers/teachers_controller_spec.rb
@@ -1,6 +1,9 @@
 require "rails_helper"
 
 RSpec.describe TeachersController do
+  let(:teacher) { create(:teacher, name: "Teacher1") }
+  let(:form) { create(:form, name: "Form1") }
+
   describe "#index" do
     it "renders the index page" do
       get :index
@@ -9,10 +12,54 @@ RSpec.describe TeachersController do
     end
 
     it "list all forms belonging to a teacher" do
-      form = create(:Form, name: Form1)
+      teacher.forms << form
       get :index
 
       expect(assigns(:forms)).to have_content "Form1"
+    end
+  end
+
+  describe "#new" do
+    it "renders the new template" do
+      get :new
+
+      expect(response).to render_template :new
+    end
+
+    it "assigns the data" do
+      get :new
+      
+      expect(assigns(:form)).to be_a_new(Form)
+    end
+  end
+
+  describe "#create" do
+    context "with valid data" do
+      it "redirects to the root path" do
+        post: create, form: attributes_for(:form, name: "Form3")
+
+        expect(response).to redirect_to root_path
+      end
+
+      it "creates a new form in the database" do
+        expect{
+          post :create, form: attributes_for(:form, name: "Form3")
+        }.to change(Form, :count).by 1
+      end
+    end
+
+    context "with invalid data" do
+      it "renders the new template" do
+        post: create, form: attributes_for(:form, name: "")
+
+        expect(response).to render_template :new
+      end
+
+      it "does not create a new form in the database" do
+        expect{
+          post :create, form: attributes_for(:form, name: "")
+        }.not_to change(Form, :count)
+      end
     end
   end
 end
