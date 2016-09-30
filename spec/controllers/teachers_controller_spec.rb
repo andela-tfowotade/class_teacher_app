84b09695ae40df719f028b69f43fc2f4d1ea6a0a
temifowotade <temitope.fowotade@andela.com>
2016-09-30 09:46:14 +0100

    test(home page controller): adds testing for index controller

--- spec/controllers/teachers_controller_spec.rb
+++ spec/controllers/teachers_controller_spec.rb
@@ -4,6 +4,8 @@ RSpec.describe TeachersController do
   describe "#index" do
     it "renders the index page" do
       get :index
+
+      expect(response).to render_template :index
     end
   end
 end
