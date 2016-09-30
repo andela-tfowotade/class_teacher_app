147df5f7d2467ce1eaacc302a24f62dc5d09834a
temifowotade <temitope.fowotade@andela.com>
2016-09-30 10:43:40 +0100

    test(creating form): Adds feature testing for creating a from

---
+++ spec/features/creating_forms_spec.rb
@@ -0,0 +1,21 @@
+require 'rails_helper' 
+
+feature "Creating Form" do
+  before do
+   visit root_path 
+   click_on "New Form"
+ end
+
+  scenario "user supplies valid data" do
+    fill_in("Name", with "My Form 1")
+    click_on("Submit")
+
+    expect(page).to have_content("My Form 1")
+  end
+
+  scenario "user supplies invalid data" do
+     click_on("Submit")
+
+     expect(page).to have_content("Can't be blank")
+  end
+end
